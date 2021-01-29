c70c30f2680b96fc813402003e565ba4d0dff7d2
Aetrix27 <davedogg323@gmail.com>
2021-01-28 16:02:48 -0800

    Challenge 1 completed

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -5,7 +5,8 @@
  * 
  * 1. Read over the code that follows. In what order will the outputs "Step 1",
  *    "Step 2", and "Step 3" be printed? How do you know?
- * 
+ *  Step 1, Step 3, and Step 2 since there is a timeout for 2000 milliseconds that delays 
+ *  the console.log
  * 
  * 2. Run this code using `node challenge1.js`. In what order were the steps
  *    printed?
@@ -14,7 +15,7 @@
  * 3. Change the delay time in the `snooze` function from 2000 ms to 0. In what
  *    order will the steps be printed now? Why? Re-run the code again to verify
  *    your expectation. Were you correct?
- * 
+ *  They will be printed in normal order: Step 1, 2, then 3.
  *******************************************************************************
  */
 
@@ -22,7 +23,7 @@
 function snooze(action) {
     setTimeout(function() {
       action();
-    }, 2000);
+    }, 0);
 }
 console.log('Step 1');
 
